d3a55f4b3b9952d2498991ea8d1d5fedcf3d69a0
Sangwon <lsw91.main@gmail.com>
2022-09-24 12:44:34 +0900

    clean dataset.py

--- deep_time_series/dataset.py
+++ deep_time_series/dataset.py
@@ -1,108 +1,17 @@
-from collections import defaultdict
-
 import numpy as np
 import pandas as pd
 
-import torch
 from torch.utils.data import Dataset
-
-from deep_time_series.transform import ColumnTransformer
 
 from .chunk import BaseChunkSpec, ChunkExtractor
 from .plotting import plot_chunks
 
 
-class Rescaler:
-    def __init__(
-        self,
-        chunk_specs: BaseChunkSpec,
-        column_transformer: ColumnTransformer,
-    ):
-        self.tag_to_names = {
-            chunk_spec.tag.split('.')[1]: chunk_spec.names
-            for chunk_spec in chunk_specs
-        }
-        self.column_transformer = column_transformer
-
-    def __call__(
-        self,
-        data: dict[str, torch.Tensor],
-        sample_index_name='sample_index',
-        time_index_name='time_index',
-        neglect_suffix=False,
-    ):
-        outputs = {}
-        for k, v in data.items():
-            if isinstance(v, torch.Tensor):
-                v = v.cpu().numpy()
-
-            tokens = k.split('.')
-            # Neglect items with suffix.
-            if len(tokens) != 2 and (not neglect_suffix):
-                continue
-
-            # Extract core tag.
-            tag = tokens[1]
-
-            # Neglect unexpected tags.
-            if tag not in self.tag_to_names:
-                continue
-
-            names = self.tag_to_names[tag]
-
-            dfs = []
-            for i, y in enumerate(v):
-                df = pd.DataFrame(data={
-                    name: values for name, values in zip(names, y.T)
-                })
-                df = self.column_transformer.inverse_transform(df)
-
-                df[sample_index_name] = i
-                df[time_index_name] = np.arange(len(df))
-                dfs.append(df)
-
-            df = pd.concat(dfs)
-            df = df.set_index([sample_index_name, time_index_name])
-
-            outputs[k] = df
-
-        return outputs
-
-    def rescale_batches(
-        self,
-        data_list: list[dict[str, torch.Tensor]],
-        batch_index_name='batch_index',
-        sample_index_name='sample_index',
-        time_index_name='time_index',
-        neglect_suffix=False,
-    ):
-        batch_outputs = defaultdict(list)
-        for i, data in enumerate(data_list):
-            outputs = self(
-                data=data,
-                sample_index_name=sample_index_name,
-                time_index_name=time_index_name,
-                neglect_suffix=neglect_suffix
-            )
-
-            for k, v in outputs.items():
-                v['batch_index'] = i
-                batch_outputs[k].append(v)
-
-        for k, v in batch_outputs.items():
-            batch_outputs[k] = pd.concat(v).reset_index().set_index(
-                [batch_index_name, sample_index_name, time_index_name]
-            )
-
-        return dict(batch_outputs)
-
-
-
 class TimeSeriesDataset(Dataset):
     def __init__(self,
         data_frames: pd.DataFrame | list[pd.DataFrame],
-        chunk_specs,
-        return_time_index=False,
+        chunk_specs: list[BaseChunkSpec],
+        return_time_index: bool = False,
     ):
         if isinstance(data_frames, pd.DataFrame):
             data_frames = [data_frames]
